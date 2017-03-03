d9f4722db3397f9549df5f3a9dba7fe501bf62a7
tbutyl <ebmiller@ucdavis.edu>
2017-03-02 17:59:01 -0800

    slo sorter trials

--- sorters/SLO_sorter_efficient.py
+++ sorters/SLO_sorter_efficient.py
@@ -3,35 +3,37 @@ import os
 import sys
 import shutil
 
+sp = os.sep
+
 def sorting(source):
         file_list = os.listdir(source)
         print (source)
-        destination_apd1 = source + "/reflectance/" #the trailing / is very important to allow the try/except statements to work. without it, files are sequentially overwritten
-        destination_apd2 = source + "/fluorescence/"
+        destination_apd1 = source + sp + "reflectance"# + sp #the trailing / is very important to allow the try/except statements to work. without it, files are sequentially overwritten
+        destination_apd2 = source + sp + "fluorescence"# +sp
 
         for each_file in file_list:
-                if os.path.isfile(source + '/' + each_file) is True:
+                if os.path.isfile(source + sp + each_file) is True:
                         if "APD1" in each_file:
                                 try:
-                                    shutil.move(source + '/' + each_file, destination_apd1)       
+                                    shutil.move(source + sp + each_file+sp, destination_apd1)       
                                 except:
                                     os.makedirs(destination_apd1)
-                                    shutil.move(source + '/' + each_file, destination_apd1)       
+                                    shutil.move(source + sp + each_file, destination_apd1)       
                         elif "APD2" in each_file:
                                 try:
-                                    shutil.move(source + '/' + each_file, destination_apd2)
+                                    shutil.move(source + sp + each_file+sp, destination_apd2)
                                 except:
                                     os.makedirs(destination_apd2)
-                                    shutil.move(source + '/' + each_file, destination_apd2)
-                elif os.path.isdir(source + '/' + each_file) is True:
-                        if source+'/'+each_file == source+'/fluorescence' or source+'/'+each_file == source+'/reflectance':
+                                    shutil.move(source + sp + each_file, destination_apd2)
+                elif os.path.isdir(source + sp + each_file) is True:
+                        if source+sp+each_file == source+sp+"fluorescence" or source+sp+each_file == source+sp+"reflectance":
                                 continue
                         else:
-                                new_source = source + '/' + each_file
+                                new_source = source + sp + each_file
                                 sorting(new_source)
 
 def main():
-    source = askdirectory()
+    source = os.path.abspath(askdirectory()) #the abspath should clean up the mixed file separators
     sorting(source)
     print("Completed")
 main()
