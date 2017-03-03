6add4e27c73ebf23aa6b24e0646693c01b5f3d03
Eric Miller <emill18@u.rochester.edu>
2017-03-03 08:38:14 -0800

    working efficient sorter on mac

--- sorters/SLO_sorter_efficient.py
+++ sorters/SLO_sorter_efficient.py
@@ -8,20 +8,20 @@ sp = os.sep
 def sorting(source):
         file_list = os.listdir(source)
         print (source)
-        destination_apd1 = source + sp + "reflectance"# + sp #the trailing / is very important to allow the try/except statements to work. without it, files are sequentially overwritten
-        destination_apd2 = source + sp + "fluorescence"# +sp
+        destination_apd1 = source + sp + "reflectance" + sp #the trailing / is very important to allow the try/except statements to work. without it, files are sequentially overwritten
+        destination_apd2 = source + sp + "fluorescence" +sp
 
         for each_file in file_list:
                 if os.path.isfile(source + sp + each_file) is True:
                         if "APD1" in each_file:
                                 try:
-                                    shutil.move(source + sp + each_file+sp, destination_apd1)       
+                                    shutil.move(source + sp + each_file, destination_apd1)       
                                 except:
                                     os.makedirs(destination_apd1)
                                     shutil.move(source + sp + each_file, destination_apd1)       
                         elif "APD2" in each_file:
                                 try:
-                                    shutil.move(source + sp + each_file+sp, destination_apd2)
+                                    shutil.move(source + sp + each_file, destination_apd2)
                                 except:
                                     os.makedirs(destination_apd2)
                                     shutil.move(source + sp + each_file, destination_apd2)
